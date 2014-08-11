e281b616e996075e86fdc3935e8e3aa9413cf706
ajlopez <webmaster@ajlopez.com>
2014-08-11 17:19:48 -0300

    Click first test

---
+++ test/click.js
@@ -0,0 +1,13 @@
+
+var sjq = require('..');
+var $ = sjq.$;
+
+exports['click function'] = function (test) {
+    test.async();
+
+    var element = $("<div>").click(function () {
+        test.done();
+    }).html("hello");
+    
+    element.$.doClick();
+};
